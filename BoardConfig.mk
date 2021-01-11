3656b9879f8e133d33f4c23480550220c1a9edfb
GeopJr <geopjr@geopjr.xyz>
2021-01-11 15:34:16 +0000

    SHRP: prepare testing build

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -129,7 +129,7 @@ SHRP_PATH := device/xiaomi/davinci
 SHRP_MAINTAINER := GeopJr
 SHRP_DEVICE_CODE := davinci
 SHRP_EDL_MODE := 1
-SHRP_EXTERNAL := /external_sd
+# SHRP_EXTERNAL := /external_sd
 SHRP_INTERNAL := /sdcard
 SHRP_OTG := /usb_otg
 SHRP_FLASH := 1
@@ -138,13 +138,13 @@ SHRP_FONP_2 := /sys/class/leds/led:torch_1/brightness
 SHRP_FONP_3 := /sys/class/leds/led:switch_0/brightness
 SHRP_FLASH_MAX_BRIGHTNESS := 200
 SHRP_REC := /dev/block/bootdevice/by-name/recovery
-SHRP_REC_TYPE := Treble
+SHRP_REC_TYPE := Normal
 SHRP_DEVICE_TYPE := A_Only
 SHRP_STATUSBAR_RIGHT_PADDING := 30
 SHRP_STATUSBAR_LEFT_PADDING := 40
 SHRP_EXPRESS := true
 SHRP_DARK := true
-SHRP_OFFICIAL := true
+# SHRP_OFFICIAL := true
 
 # Hack: prevent anti rollback
 PLATFORM_SECURITY_PATCH := 2099-12-31
